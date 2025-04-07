2ccd8090186db9f97f5313171b00448ca0337383
lPhiNix <pablomargamer05@gmail.com>
2025-04-07 14:38:59 +0200

    Session and Game Commented

--- src/main/java/org/phinix/lib/server/session/Session.java
+++ src/main/java/org/phinix/lib/server/session/Session.java
@@ -2,9 +2,34 @@ package org.phinix.lib.server.session;
 
 import org.phinix.lib.server.core.Manageable;
 
+/**
+ * Interface representing a session that can be managed.
+ *
+ * @see Manageable
+ */
 public interface Session extends Manageable {
+
+    /**
+     * Starts the session.
+     */
     void start();
+
+    /**
+     * Checks if the session has ended.
+     *
+     * @return {@code true} if the session has ended, {@code false} otherwise
+     */
     boolean isEnd();
+
+    /**
+     * Sets the session end state.
+     *
+     * @param isEnd the session end state
+     */
     void setIsEnd(boolean isEnd);
+
+    /**
+     * Ends the session.
+     */
     void end();
 }
--- src/main/java/org/phinix/lib/server/session/game/Game.java
+++ src/main/java/org/phinix/lib/server/session/game/Game.java
@@ -3,8 +3,29 @@ package org.phinix.lib.server.session.game;
 import org.phinix.lib.server.session.Session;
 import org.phinix.lib.server.core.worker.Worker;
 
+/**
+ * Interface representing a game session.
+ *
+ * @see Session
+ */
 public interface Game extends Session {
+
+    /**
+     * Returns the worker whose turn it is.
+     *
+     * @return the worker whose turn it is
+     */
     Worker isTurn();
+
+    /**
+     * Checks if the game is over.
+     */
     void checkGameOver();
+
+    /**
+     * Handles the disconnection of a client.
+     *
+     * @param client the client that disconnected
+     */
     void handleDisconnect(Worker client);
 }
